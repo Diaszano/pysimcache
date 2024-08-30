eff6e1b46b86cfe6673862cae5aea2081d17f2aa
Diaszano <61257292+Diaszano@users.noreply.github.com>
2024-08-30 18:41:20 -0300

    feat: created initial cli

---
+++ app/cli.py
@@ -0,0 +1,21 @@
+from typer import Typer, Argument
+from typing_extensions import Annotated
+
+from .replacement_policy import ReplacementPolicyType
+
+app = Typer()
+
+
+@app.command()
+def main(
+        nsets: Annotated[int, Argument(min=0)],
+        bsize: Annotated[int, Argument(min=0)],
+        assoc: Annotated[int, Argument(min=0)],
+        policy: ReplacementPolicyType,
+        output: Annotated[int, Argument(min=0, max=1)],
+        file: str
+    ) -> None:
+    pass
+
+
+app()
