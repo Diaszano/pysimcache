a46eb21fd33fb053a48b92e10f95a904f9335c31
Diaszano <61257292+Diaszano@users.noreply.github.com>
2024-08-30 18:26:29 -0300

    feat: exported modules

---
+++ app/replacement_policy/__init__.py
@@ -0,0 +1,4 @@
+from .replacement_policy_types import ReplacementPolicyType
+from .random_replacement_policy import RandomReplacementPolicy
+
+__all__ = ("ReplacementPolicyType", "RandomReplacementPolicy")
