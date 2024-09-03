dfa81d46fd1c2b921e2a8702da13de49292ea04d
Diaszano <61257292+Diaszano@users.noreply.github.com>
2024-09-02 22:29:57 -0300

    feat: past the lint in the file

--- app/cli.py
+++ app/cli.py
@@ -1,4 +1,4 @@
-from typer import Typer, Argument
+from typer import Argument, Typer
 from typing_extensions import Annotated
 
 from .replacement_policy import ReplacementPolicyType
@@ -8,14 +8,14 @@ app = Typer()
 
 @app.command()
 def main(
-        nsets: Annotated[int, Argument(min=0)],
-        bsize: Annotated[int, Argument(min=0)],
-        assoc: Annotated[int, Argument(min=0)],
-        policy: ReplacementPolicyType,
-        output: Annotated[int, Argument(min=0, max=1)],
-        file: str
-    ) -> None:
-    pass
+	nsets: Annotated[int, Argument(min=0)],
+	bsize: Annotated[int, Argument(min=0)],
+	assoc: Annotated[int, Argument(min=0)],
+	policy: ReplacementPolicyType,
+	output: Annotated[int, Argument(min=0, max=1)],
+	file: str,
+) -> None:
+	pass
 
 
 app()
