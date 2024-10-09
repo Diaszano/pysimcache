60c83459ef57f60159d88831f32a6d100c71bab4
Diaszano <61257292+Diaszano@users.noreply.github.com>
2024-10-09 14:16:02 -0300

    chore: renamed from cache_mapeamento.py to cache_mapping.py

--- app/cache/__init__.py
+++ app/cache/__init__.py
@@ -1,5 +1,5 @@
 from .cache_block import Block
-from .cache_mapeamento import CacheMapping
+from .cache_mapping import CacheMapping
 from .cache_set import Set
 
 __all__ = ('Block', 'Set', 'CacheMapping')
