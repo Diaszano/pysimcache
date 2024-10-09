b5c2fae7142698c08d17ec826a97c77d8fe3c67f
Diaszano <61257292+Diaszano@users.noreply.github.com>
2024-10-09 19:24:00 -0300

    feat: exported class ReplacementPolicy

--- app/replacement_policy/__init__.py
+++ app/replacement_policy/__init__.py
@@ -1,6 +1,7 @@
 from .fifo_replacement_policy import FIFO
 from .lru_replacement_policy import LRU
 from .random_replacement_policy import Random
+from .replacement_policy import ReplacementPolicy
 from .replacement_policy_types import ReplacementPolicyType
 
 __all__ = (
@@ -8,4 +9,5 @@ __all__ = (
 	'LRU',
 	'Random',
 	'ReplacementPolicyType',
+	'ReplacementPolicy',
 )
